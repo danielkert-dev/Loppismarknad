a27a1a0237ea810d431fc5d885472f5bca78c1e5
Daniel Kert <danielkertdev@gmail.com>
2023-06-14 00:51:21 +0300

    commit may 14 00:50 23
    Lagade mer index och satte category. footer, header. Ska göra auth imorgon

--- mysite/settings.py
+++ mysite/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'core',
+    'item',
 ]
 
 MIDDLEWARE = [
@@ -135,6 +136,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
