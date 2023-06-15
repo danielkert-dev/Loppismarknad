ac862b3d94f148fed41152ec7351da732375acaa
Daniel Kert <danielkertdev@gmail.com>
2023-06-15 20:25:46 +0300

    23 06 15 20:25

---
+++ item/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+
+from .models import Item
+
+
+class NewItemForm(forms.ModelForm):
+    class Meta:
+        model = Item
+        fields = ('category', 'name', 'description', 'price', 'image')
+
+        widgets = {
+            'category': forms.Select(attrs={'class': 'form-control'}),
+            'name': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+            'price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'image': forms.FileInput(attrs={'class': 'form-control'}),
+        }
