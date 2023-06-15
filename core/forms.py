ca49d311143808912f7cf402c59ae0bf78453241
Daniel Kert <danielkertdev@gmail.com>
2023-06-15 18:07:48 +0300

    commit fix again 23 06 15 18:07

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,23 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder': 'Your username',
+        'class': 'form-control'
+    }))
+
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder': 'Your email',
+        'class': 'form-control'
+    }))
+
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Your password',
+        'class': 'form-control'
+    }))
+
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Repeat password',
+        'class': 'form-control'
+    }))
